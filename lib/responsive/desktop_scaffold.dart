39ac4dbf16261d21fe1ed88793e78bfedcae1953
Samuel Ebebenge <samuel.ebebenge@gmail.com>
2022-10-05 11:27:43 +0200

    added tablet and mobile layout

--- lib/responsive/desktop_scaffold.dart
+++ lib/responsive/desktop_scaffold.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_responsiveness/constants.dart';
+import 'package:flutter_responsiveness/util/custom_tile.dart';
+import 'package:flutter_responsiveness/util/title_text.dart';
+import 'package:flutter_responsiveness/util/top_box.dart';
 
 class DesktopScaffold extends StatefulWidget {
   const DesktopScaffold({Key? key}) : super(key: key);
@@ -18,7 +21,49 @@ class _DesktopScaffoldState extends State<DesktopScaffold> {
         // open drawer
         myDrawer,
         //rest of the body
-        Container()
+        Expanded(
+          flex: 2,
+          child: Column(children: [
+            TitleText(title: 'Featured content on Desktop'),
+            // 4 boxes on the top
+            AspectRatio(
+              aspectRatio: 4,
+              child: SizedBox(
+                width: double.infinity,
+                child: GridView.builder(
+                  primary:
+                      false, // Error fixed : ScrollController is currently attached to more than one ScrollPosition.
+                  itemCount: 4,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 4),
+                  itemBuilder: (context, index) {
+                    return TopBox();
+                  },
+                ),
+              ),
+            ),
+            SizedBox(height: 12),
+            TitleText(title: 'Recommended for you'),
+            // tiles on the bottom
+            Expanded(
+              child: ListView.builder(
+                itemCount: 7,
+                itemBuilder: (context, index) {
+                  return CustomTile();
+                },
+              ),
+            )
+          ]),
+        ),
+        Expanded(
+            child: Column(
+          children: [
+            Expanded(
+                child: Container(
+              color: Colors.blueGrey,
+            ))
+          ],
+        ))
       ]),
     );
   }
